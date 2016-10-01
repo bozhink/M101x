6144bd504ec3aab381661520dd01cc3e0d24b3da
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-01 16:12:24 +0300

    chapter-2/homework is implemented

--- chapter-2/homework/student.js
+++ chapter-2/homework/student.js
@@ -8,15 +8,17 @@ var schema = new mongoose.Schema({
 /* Returns the student's first name, which we will define
  * to be everything up to the first space in the student's name.
  * For instance, "William Bruce Bailey" -> "William" */
-schema.virtual('firstName').get(function() {
-  return 'Not Implemented!';
+schema.virtual('firstName').get(function () {
+  var nameParts = this.name.split(' ');
+  return nameParts[0];
 });
 
 /* Returns the student's last name, which we will define
  * to be everything after the last space in the student's name.
  * For instance, "William Bruce Bailey" -> "Bailey" */
 schema.virtual('lastName').get(function() {
-  return 'Not Implemented!';
+  var nameParts = this.name.split(' ');
+  return nameParts[nameParts.length - 1];
 });
 
 module.exports = schema;
