88f4bf913b163bd571d6f20e466f43f28a580c69
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-17 22:32:28 +0300

    Config is congiured in hw-3

--- chapter-3/homework/dependencies.js
+++ chapter-3/homework/dependencies.js
@@ -2,18 +2,20 @@ var fs = require('fs');
 var fx = require('./fx');
 var Stripe = require('stripe');
 
-module.exports = function(wagner) {
+module.exports = function (wagner) {
   var stripe =
 
-  // TODO: Make Stripe depend on the Config service and use its `stripeKey`
-  // property to get the Stripe API key.
-  wagner.factory('Stripe', function() {
-    return Stripe(process.env.STRIPE_API_KEY);
-  });
+    // TODO: Make Stripe depend on the Config service and use its `stripeKey`
+    // property to get the Stripe API key.
+    wagner.factory('Stripe', function () {
+      return Stripe(wagner.invoke(function (Config) {
+        return Config.stripeKey;
+      }));
+    });
 
   wagner.factory('fx', fx);
 
-  wagner.factory('Config', function() {
-    return JSON.parse(fs.readFileSync('./config.json').toString());
+  wagner.factory('Config', function () {
+    return JSON.parse(fs.readFileSync('~/tmp/m101x/config.json').toString());
   });
 };
