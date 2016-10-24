bb22318377fe80de7ca2661da74574fe9383e90c
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-24 23:27:00 +0300

    chapter4/homework is implemented

--- chapter-4/homework/bin/index.js
+++ chapter-4/homework/bin/index.js
@@ -132,10 +132,8 @@ exports.SearchBarController = function($scope, $http) {
   // `/api/v1/product/text/:searchText` and expose the response's
   // `products` property as `results` to the scope.
   $scope.update = function() {
-    var q = encodeURIComponent($scope.searchText);
-    $http.
-      get('/api/v1/product/text/' + q).
-      success(function(data) {
+    $http.get('/api/v1/product/text/' + $scope.searchText)
+      .success(function (data) {
         $scope.results = data.products;
       });
   };
--- chapter-4/homework/controllers.js
+++ chapter-4/homework/controllers.js
@@ -131,6 +131,10 @@ exports.SearchBarController = function($scope, $http) {
   // `/api/v1/product/text/:searchText` and expose the response's
   // `products` property as `results` to the scope.
   $scope.update = function() {
+    $http.get('/api/v1/product/text/' + $scope.searchText)
+      .success(function (data) {
+        $scope.results = data.products;
+      });
   };
 
   setTimeout(function() {
